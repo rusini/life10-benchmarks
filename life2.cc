52ef2aa1a5f6336e8f8fcfeccb5980a9e2c47c46
rusini <rusini@manool.org>
2020-12-29 19:34:19 -0500

    Add alternative C++ versions

--- life2.cc
+++ life2.cc
@@ -1,4 +1,5 @@
-// life.cc -- Conway's Game of Life in C++ (version 2011 and later)
+// life2.cc -- Conway's Game of Life in C++ (version 2011 and later)
+// facilitate bounds checking elimination
 
 # include <vector>
 # include <iostream>
@@ -29,12 +30,10 @@ int main() {
 
    auto nextb = std::vector<std::vector<signed char>>(n, std::vector<signed char>(m));
    for (int k = 0; k < g; ++k) {
-      for (int i = 0; i < n; ++i) {
-         constexpr int nm1 = n - 1;
-         int up = i != 0 ? i - 1 : nm1, down = i != nm1 ? i + 1 : 0;
-         for (int j = 0; j < m; ++j) {
-            constexpr int mm1 = m - 1;
-            int left = j != 0 ? j - 1 : mm1, right = j != mm1 ? j + 1 : 0;
+      for (decltype(b.size()) i = 0; i < b.size(); ++i) {
+         auto up = i != 0 ? i - 1 : b.size() - 1, down = i != b.size() - 1 ? i + 1 : 0;
+         for (decltype(b.at(i).size()) j = 0; j < b.at(i).size(); ++j) {
+            auto left = j != 0 ? j - 1 : b.at(i).size() - 1, right = j != b.at(i).size() - 1 ? j + 1 : 0;
             int count =
                b.at(up  ).at(left ) +
                b.at(up  ).at(j    ) +
